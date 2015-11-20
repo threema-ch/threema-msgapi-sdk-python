f0e71bdeca1a553c05228b57366a46c25db3d632
Lennart Grahl <lennart.grahl@gmail.com>
2015-11-20 23:23:14 +0100

    Add missing docstring for read_key_or_key_file

--- threema/gateway/util.py
+++ threema/gateway/util.py
@@ -7,6 +7,17 @@ __all__ = ('read_key_or_key_file',)
 
 
 def read_key_or_key_file(key, expected_type):
+    """
+    Decode a hex-encoded key or read it from a file.
+
+    Arguments:
+        - `key`: A hex-encoded key or the name of a file which contains
+          a key.
+        - `expected_type`: One of the types of :class:`Key.Type`.
+
+    Return a:class:`libnacl.public.SecretKey` or
+    :class:`libnacl.public.PublicKey` instance.
+    """
     # Read key file (if any)
     try:
         with open(key) as file:
