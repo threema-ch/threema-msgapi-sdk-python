dab93282ab909d24d22860d4dd7a20f1d170c318
Lennart Grahl <lennart.grahl@gmail.com>
2017-09-22 00:52:09 +0200

    Release v3.0.6

--- threema/gateway/__init__.py
+++ threema/gateway/__init__.py
@@ -30,7 +30,7 @@ from .exception import *  # noqa
 
 __author__ = 'Lennart Grahl <lennart.grahl@gmail.com>'
 __status__ = 'Production'
-__version__ = '3.0.5'
+__version__ = '3.0.6'
 feature_level = 3
 
 __all__ = tuple(itertools.chain(
